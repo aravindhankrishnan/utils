ed766c0b7d497566231cdc52694c341b48786c3c
Aravindhan <aravindhan.krishnan@asu.edu>
2013-12-26 22:51:23 -0700

    adding test function for getfilename()

--- test.cpp
+++ test.cpp
@@ -2,13 +2,21 @@
 using namespace std;
 
 #include "timer.h"
+#include "getfilename.h"
 
 int main (int argc, char *argv[])
 {
-  string timer_resolution = "microseconds";
-  utils::Timer timer (timer_resolution);
-  timer.tic ();
-  sleep (3);
-  cout << timer.toc () << " " << timer_resolution << endl;
+
+  string basename = "test";
+  string extension = "txt";
+
+  cout << utils::getfilename (basename, extension) << endl;
+
+
+  //string timer_resolution = "microseconds";
+  //utils::Timer timer (timer_resolution);
+  //timer.tic ();
+  //sleep (3);
+  //cout << timer.toc () << " " << timer_resolution << endl;
   return 0;
 }
